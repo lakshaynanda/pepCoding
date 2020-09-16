f7d70991fa036d98927701d8531fb7955f396044
lakshaynanda <lakshay.nanda08@gmail.com>
2020-09-17 00:21:41 +0530

    Patterns 1,2,3,4

--- Website Ques /Foundation/Patterns/pattern4.java
+++ Website Ques /Foundation/Patterns/pattern4.java
@@ -1,25 +1,23 @@
 import java.util.*;
 
-public class pattern3 {
+public class pattern4 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-
-        int nsp = n - 1;
-        int nst = 1;
+        int cst = n;
+        int csp = 0;
+        // write ur code here
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= nsp; j++) {
+            for (int j = 1; j <= csp; j++) {
                 System.out.print("\t");
-
             }
-            for (int k = 1; k <= nst; k++) {
+            for (int k = 1; k <= cst; k++) {
                 System.out.print("*\t");
             }
-            nst++;
-            nsp--;
             System.out.println();
+            cst--;
+            csp++;
         }
-        // write ur code here
     }
 }
