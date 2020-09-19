049a8a5ec6912faa518cc8bf4e6a20795c6948db
lakshaynanda <lakshay.nanda08@gmail.com>
2020-09-19 15:05:24 +0530

    Patterns 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20

--- Website Ques /Foundation/Patterns/pattern20.java
+++ Website Ques /Foundation/Patterns/pattern20.java
@@ -5,20 +5,20 @@ public class pattern20 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int st=2;
-        int sp=3;
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(j==1 || j==n){
+        int st = 2;
+        int sp = 3;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || j == n) {
                     System.out.print("*\t");
-                }
-                else if(i>n/2 && (i==j ||i+j==n+1)){
+                } else if (i > n / 2 && (i == j || i + j == n + 1)) {
                     System.out.print("*\t");
-                }else{
+                } else {
                     System.out.print("\t");
                 }
-            }System.out.println();
+            }
+            System.out.println();
         }
         // write ur code here
 
