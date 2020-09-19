e227d3325c0b064ac212a2a08f03fa1fdee6457e
lakshaynanda <lakshay.nanda08@gmail.com>
2020-09-19 21:44:03 +0530

    Any Base to Any Base Conversion

---
+++ Website Ques /Foundation/Functions and Arrays/anyBasetoAnybase.java
@@ -0,0 +1,25 @@
+import java.util.*;
+  
+  public class anyBasetoAnybase{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int dn = getValueInBase(n, b);
+      System.out.println(dn);
+   }
+  
+   public static int getValueInBase(int n, int b){
+       // write code here
+       int rv = 0;
+       int p = 1;
+       while(n>0){
+           int rem = n % b;
+           n = n / b;
+           rv += rem*p;
+           p = p * 10;
+       }
+       return rv;
+   }
+  }
