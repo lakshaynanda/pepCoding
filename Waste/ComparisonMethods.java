1569bfc3e6a05eb0d3cf4668ebc3e69861989c22
lakshaynanda <lakshay.nanda08@gmail.com>
2020-09-26 20:19:28 +0530

    Waste Updated

--- Waste/ComparisonMethods.java
+++ Waste/ComparisonMethods.java
@@ -3,57 +3,73 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Benchmarker{
-  
-  
-  public static String compare(long n) throws RuntimeException{
-    String str = new String();
-    long start1 = System.currentTimeMillis();
-    long val1 = ComparisonMethods.bruteForce(n);
-    long end1 = System.currentTimeMillis();
-    
-    long start2 = System.currentTimeMillis();
-    long val2 = ComparisonMethods.constantTime(n);
-    long end2 = System.currentTimeMillis();
-
-    long bruteForceTime = end1-start1;
-    long formulaTime = end2-start2;
-
-    str = n + "\t" + bruteForceTime +"\t" + formulaTime +"\n";
-    if(val1 != val2) {
-        str = "Values don't match";
-        throw new NoSuchElementException(str);
+public class ComparisonMethods{
+    public static long bruteForce(long n){
+        /**
+        * Complexity: O(N)
+        */
+        long sum = 0;
+        long i = 1;
+        while(i<=n){
+            sum = sum + i;
+            i++;
+        }
+        return sum;
     }
-    return str;
-}
+    public static long constantTime(long n){
+        /**
+        * Complexity: O(1)
+        */
+        long sum = n*(n+1)/2;
+        return sum;
+    }
+    public static String compare(long n) throws RuntimeException{
+        String str = new String();
+        long start1 = System.currentTimeMillis();
+        long val1 = bruteForce(n);
+        long end1 = System.currentTimeMillis();
+        
+        long start2 = System.currentTimeMillis();
+        long val2 = constantTime(n);
+        long end2 = System.currentTimeMillis();
 
+        long bruteForceTime = end1-start1;
+        long formulaTime = end2-start2;
+
+        str = n + "\t" + bruteForceTime +"\t" + formulaTime +"\n";
+        if(val1 != val2) {
+            str = "Values don't match";
+            throw new NoSuchElementException(str);
+        }
+        return str;
+    }
     public static void createResultsFile(File f, long[] queryNs){
 
-      FileWriter fr = null;
-      String str = new String();
-      try {
-          fr = new FileWriter(f);
-      } catch (IOException e){
-          System.out.println("Exception encountered, unable to complete method");
-      }
-      try{
-          for(int i=0;i<queryNs.length;i++){
-              try{
-                  str = compare(queryNs[i]);
-              } catch(NoSuchElementException e){
-                  e.printStackTrace();
-              }
-              fr.write(str);
-          }
-      }catch (IOException e){
-              System.out.println("Exception encountered while writing for value N = " + str);
-          }
-      try {
-          fr.close();
-      } catch (IOException e) {
-          System.out.println("Exception encountered while closing file");
-      }
-  }
+        FileWriter fr = null;
+        String str = new String();
+        try {
+            fr = new FileWriter(f);
+        } catch (IOException e){
+            System.out.println("Exception encountered, unable to complete method");
+        }
+        try{
+            for(int i=0;i<queryNs.length;i++){
+                try{
+                    str = compare(queryNs[i]);
+                } catch(NoSuchElementException e){
+                    e.printStackTrace();
+                }
+                fr.write(str);
+            }
+        }catch (IOException e){
+                System.out.println("Exception encountered while writing for value N = " + str);
+            }
+        try {
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("Exception encountered while closing file");
+        }
+    }
     
     public static void main(String args[]){
         long[] queryNs = new long[5];
@@ -65,5 +81,6 @@ public class Benchmarker{
 
         File f = new File("/Users/Shivam/source.txt");
         createResultsFile(f,queryNs);
+
     }
 }
