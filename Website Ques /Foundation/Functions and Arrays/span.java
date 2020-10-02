63575219c27c33060b14c0ab468edadd6a6ea514
lakshaynanda <lakshay.nanda08@gmail.com>
2020-10-02 21:30:30 +0530

    span

---
+++ Website Ques /Foundation/Functions and Arrays/span.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class span{
+
+public static int maxi(int[] arr){
+    int max = arr[0];
+    for(int i=1;i<arr.length;i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+public static int mini(int[] arr){
+    int min = arr[0];
+    for(int i=1;i<arr.length;i++){
+        if(arr[i]<min){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    int arr[] = new int[n];
+    for(int i=0;i<arr.length;i++){
+        arr[i] = sc.nextInt();
+    }
+    int span = maxi(arr)-mini(arr);
+    System.out.println(span);
+    
+ }
+
+}
