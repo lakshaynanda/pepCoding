26424075c9780d0a652b87da1979ecc0015ddf1f
lakshaynanda <lakshay.nanda08@gmail.com>
2020-10-02 22:04:13 +0530

    find in array

---
+++ Website Ques /Foundation/Functions and Arrays/findInArray.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class findInArray{
+public static void find(int a[], int d){
+    int idx = -1;
+    for(int i = 0; i < a.length; i++){
+        if(a[i] == d){
+            idx = i;
+            break;
+        }
+    }
+    System.out.println(idx);
+}
+
+public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int a[] = new int[n];
+    for(int i = 0; i < a.length; i++){
+        a[i] = sc.nextInt();
+    }
+    
+    int d = sc.nextInt();
+    
+    find(a,d);
+    // write your code here
+ }
+
+}
